f4b52b467ace1b666c780022f71563a203dfe8d6
imaginationmadi <145612004+imaginationmadi@users.noreply.github.com>
2023-09-29 04:02:34 -0700

    Twelve Days of Christmas
    This is a holly program that prints out the lyrics to the holiday classic "The Twelve Days of Christmas"
    
    Each stanza is separated by an empty line.

---
+++ Twelvedays.java
@@ -0,0 +1,45 @@
+// Chapter One Twelve Days
+// Prints the lyrics to The Twelve Days of Christmas
+
+public class Twelvedays {
+	
+	public static void main (String[] args) {
+		System.out.println ("On the first day of Christmas, \nmy true love gave to me, \na partridge in a pear tree.");
+		System.out.println ();
+		System.out.println ("On the second day of Christmas, \nmy true love gave to me, \ntwo turtle doves, and \na partridge in a pear tree.");
+		System.out.println ();
+		System.out.println ("On the third day of Christmas, \nmy true love gave to me, \nthree French Horns, \ntwo turtle doves, and \na partridge in a pear tree.");
+		System.out.println ();
+		System.out.println ("On the fourth day of Christmas, \nmy true love gave to me, \nfour calling birds, \nthree French Horns, \ntwo turtle doves, and \na partridge in a pear tree.");
+		System.out.println ();
+		System.out.println ("On the fifth day of Christmas, \nmy true love gave to me, \nFIVE GOLDEN RINGS...");
+		System.out.println ("four calling birds, \nthree French Horns, \ntwo turtle doves, and \na partridge in a pear tree.");
+		System.out.println ();
+		System.out.println ("On the sixth day of Christmas, \nmy true love gave to me, \nsix geese a-laying, \nFIVE GOLDEN RINGS... ");
+		System.out.println ("four calling birds, \nthree French Horns, \ntwo turtle doves, and \na partridge in a pear tree.");
+		System.out.println ();
+		System.out.println ("On the seventh day of Christmas, \nmy true love gave to me, \nseven swans a-swimming, \nsix geese a laying,");
+		System.out.println ("FIVE GOLDEN RINGS... \nfour calling birds, \nthree French Horns, \ntwo turtle doves, and \na partridge in a pear tree.");
+		System.out.println ();
+		System.out.println ("On the eigth day of Christmas, \nmy true love gave to me, \neight maids a-milking, \nseven swans a swimmings, \nsix geese a-laying, \nFIVE GOLDEN RINGS...");
+		System.out.println ("four calling birds, \nthree French Horns, \ntwo turtle doves, and \na partridge in a pear tree.");
+		System.out.println ();
+		System.out.println ("On the ninth day of Christmas, \nmy true love gave to me, \nnine ladies dancing,");
+		System.out.println ("eight maids a-milking, \nseven swans a swimmings, \nsix geese a-laying, \nFIVE GOLDEN RINGS...");
+		System.out.println ("four calling birds, \nthree French Horns, \ntwo turtle doves, and \na partridge in a pear tree.");
+		System.out.println ();
+		System.out.println ("On the tenth day of Christmas, \nmy true love gave to me, \nten lords a-leaping, \nnine ladies dancing,");
+		System.out.println ("eight maids a-milking, \nseven swans a swimmings, \nsix geese a-laying, \nFIVE GOLDEN RINGS...");
+		System.out.println ("four calling birds, \nthree French Horns, \ntwo turtle doves, and \na partridge in a pear tree.");
+		System.out.println ();
+		System.out.println ("On the eleventh day of Christmas, \nmy true love gave to me, \neleven pipers piping, \nten lords a-leaping, \nnine ladies dancing,");
+		System.out.println ("eight maids a-milking, \nseven swans a swimmings, \nsix geese a-laying, \nFIVE GOLDEN RINGS...");
+		System.out.println ("four calling birds, \nthree French Horns, \ntwo turtle doves, and \na partridge in a pear tree.");
+		System.out.println ();
+		System.out.println ("On the twelfth day of Christmas, \nmy true love gave to me, \ntwelve drummers drumming,");
+		System.out.println ("eleven pipers piping, \nten lords a-leaping, \nnine ladies dancing,");
+		System.out.println ("eight maids a-milking, \nseven swans a swimmings, \nsix geese a-laying, \nFIVE GOLDEN RINGS...");
+		System.out.println ("four calling birds, \nthree French Horns, \ntwo turtle doves, and \na partridge in a pear tree.");
+		
+	}
+}
